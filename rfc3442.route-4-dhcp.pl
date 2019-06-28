cf29333d0042721128491aaf64fcd657d96aa41d
stas <nsv777@gmail.com>
2019-06-28 11:38:08 +0200

    print routeros commands

--- rfc3442.route-4-dhcp.pl
+++ rfc3442.route-4-dhcp.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 use strict;
 use warnings FATAL => 'all';
@@ -9,8 +9,8 @@ my ($net, $gw, $aggregate) = ("", "", "");
 my $re_ip = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
 
 foreach (@ARGV) {
-    unless ($net) { $net = $_; next; }
-    unless ($gw) {
+    unless ($net ne '') { $net = $_; next; }
+    unless ($gw ne '') {
         $gw = $_;
 
         my ($network, $subnetmask) = split("/", $net);
@@ -46,13 +46,21 @@ foreach (@ARGV) {
             $aggregate .= sprintf("%s%s%s", $networklen, $destination, $router);
 
             printf(
-                "option_121_route_%s_via_%s : 0x%s%s%s\n",
+                "opt121_r_%s_via_%s : 0x%s%s%s\n",
                 $net, $gw, $networklen, $destination, $router
             );
             printf(
-                "option_249_route_%s_via_%s : 0x%s%s%s\n",
+                "opt249_r_%s_via_%s : 0x%s%s%s\n",
                 $net, $gw, $networklen, $destination, $router
             );
+            # printf(
+            #     "/ip dhcp-server option add code=121 name=opt121_r_%s_via_%s value=0x%s%s%s\n",
+            #     $net, $gw, $networklen, $destination, $router
+            # );
+            # printf(
+            #     "/ip dhcp-server option add code=249 name=opt249_r_%s_via_%s value=0x%s%s%s\n",
+            #     $net, $gw, $networklen, $destination, $router
+            # )
 
         } else {
             print STDERR sprintf("Mask %d, network %s or gateway %s error\n");
@@ -63,17 +71,19 @@ foreach (@ARGV) {
     $gw = "";
 }
 
-if ($aggregate) {
-    printf("aggregate_opt_121 : 0x%s\n", $aggregate);
-    printf("aggregate_opt_249 : 0x%s\n", $aggregate);
+if ($aggregate ne '') {
+    printf("aggregate_opt121 : 0x%s\n", $aggregate);
+    printf("aggregate_opt249 : 0x%s\n", $aggregate);
+    printf("/ip dhcp-server option add code=121 name=aggr_opt121 value=0x%s\n", $aggregate);
+    printf("/ip dhcp-server option add code=249 name=aggr_opt249 value=0x%s\n", $aggregate);
+    say("/ip dhcp-server option sets add name=set_121_249 options=aggr_opt121,aggr_opt249");
 }
 
 
 sub to_hex {
     my $n = shift;
-    return "00" unless $n;
+    return "00" unless defined $n;
     $n = sprintf("%x", $n);
     ($n) = "0$n" =~ m/.*(..)$/;
     return $n;
 }
-
